3b59d570732b4b31a5ee8465993b8ed2424f3048
sean <zwxbest@hotmail.com>
2025-04-08 15:39:07 +0800

    update credit inquiry method

--- api/demo/test_credit_inquiry.py
+++ api/demo/test_credit_inquiry.py
@@ -1,16 +1,16 @@
 import requests
 
-EndPoint_China = "gateway-us.svsbusiness.com"
+EndPoint_China = "gateway.svsbusiness.com"
 EndPoint_US = "gateway-us.svsbusiness.com"
 
 if __name__ == '__main__':
-    cooperator = "xxxxx"
+    cooperator = "xxxx"
     ace_token = "xxxxxxxxxxxxxxxxxxxxxx"
    
-    url = "https://{}/bill/quota/".format(EndPoint_US)
+    url = "https://{}/bill/quota/".format(EndPoint_China)
     data_dict = {
         "cooperator": cooperator,
-        "ace_token": ace_token,
+        "ace_token": ace_token
     }
     resp = requests.get(url=url, params=data_dict).text
     print(resp)
